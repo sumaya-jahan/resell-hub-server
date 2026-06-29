a658343e1e1bcdc6069511aa011fed3295b3c5df
sumaya-jahan <sumayajahan765@gmail.com>
2026-06-30 04:10:17 +0600

    Connect MongoDB Atlas

--- phAssignment10/ReSell-Hub/server/index.js
+++ phAssignment10/ReSell-Hub/server/index.js
@@ -1,16 +1,52 @@
+
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB URI
+const uri = `mongodb + srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8eggrxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create Mongo Client
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // await client.connect();
+
+        const database = client.db("resellHubDB");
+        const usersCollection = database.collection("users");
+
+        // Test MongoDB Connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("✅ Connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+// Default Route
 app.get("/", (req, res) => {
     res.send("ReSell Hub Server is Running...");
 });
 
+// Start Server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
